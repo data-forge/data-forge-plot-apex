cd417d26f554e7030ef9530203ff4ccba3a21a02
Ashley Davis <ashley@codecapers.com.au>
2019-04-18 17:51:37 +1000

    Switched back to commonjs library target after getting this working in template and module forms.

--- webpack.lib.config.js
+++ webpack.lib.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "index.js",
-        libraryTarget: "umd",
+        libraryTarget: "commonjs",
     },
 
     target: "node",
